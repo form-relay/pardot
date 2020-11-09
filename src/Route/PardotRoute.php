fef288b38bb9ac600ae50d3e9f929b1927db68b5
Stephan Ude <ude@mediatis.de>
2020-11-09 19:08:45 +0100

    [BUGFIX] fixed default route configuration

--- src/Route/PardotRoute.php
+++ src/Route/PardotRoute.php
@@ -36,46 +36,44 @@ class PardotRoute extends RequestRoute
 
     public static function getDefaultConfiguration(): array
     {
-        return parent::getDefaultConfiguration() + [
-            static::KEY_SEND_VISITOR_COOKIES => static::DEFAULT_SEND_VISITOR_COOKIES,
-            'fields' => [
-                'mapping' => [
-                    'source' => 'source',
+        $defaults = parent::getDefaultConfiguration();
+        $defaults[static::KEY_SEND_VISITOR_COOKIES] = static::DEFAULT_SEND_VISITOR_COOKIES;
+        $defaults['fields']['mapping'] = [
+            'source' => 'source',
 
-                    'salutation' => 'salutation',
-                    'first_name' => 'first_name',
-                    'last_name' => 'last_name',
+            'salutation' => 'salutation',
+            'first_name' => 'first_name',
+            'last_name' => 'last_name',
 
-                    'email' => 'email',
-                    'phone' => 'phone',
-                    'fax' => 'fax',
+            'email' => 'email',
+            'phone' => 'phone',
+            'fax' => 'fax',
 
-                    'company' => 'company',
-                    'industry' => 'industry',
-                    'department' => 'department',
-                    'job_title' => 'job_title',
-                    'website' => 'website',
-                    'annual_revenue' => 'annual_revenue',
-                    'years_in_business' => 'years_in_business',
-                    'employees' => 'employees',
+            'company' => 'company',
+            'industry' => 'industry',
+            'department' => 'department',
+            'job_title' => 'job_title',
+            'website' => 'website',
+            'annual_revenue' => 'annual_revenue',
+            'years_in_business' => 'years_in_business',
+            'employees' => 'employees',
 
-                    'address_one' => 'address_one',
-                    'address_two' => 'address_two',
-                    'zip' => 'zip',
-                    'city' => 'city',
-                    'state' => 'state',
-                    'country' => 'country',
-                    'territory' => 'territory',
+            'address_one' => 'address_one',
+            'address_two' => 'address_two',
+            'zip' => 'zip',
+            'city' => 'city',
+            'state' => 'state',
+            'country' => 'country',
+            'territory' => 'territory',
 
-                    'is_do_not_call' => 'is_do_not_call',
-                    'is_do_not_email' => 'is_do_not_email',
-                    'opted_out' => 'opted_out',
-                ],
-                'unmapped' => [
-                    SubmissionConfigurationInterface::KEY_SELF => 'comments',
-                    'appendKeyValue' => true,
-                ],
-            ],
+            'is_do_not_call' => 'is_do_not_call',
+            'is_do_not_email' => 'is_do_not_email',
+            'opted_out' => 'opted_out',
         ];
+        $defaults['fields']['unmapped'] = [
+            SubmissionConfigurationInterface::KEY_SELF => 'comments',
+            'appendKeyValue' => true,
+        ];
+        return $defaults;
     }
 }
